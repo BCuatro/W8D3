65c924acc65c6e784a450739e044b8abfc3c5b3f
Eric Balfour <egbalfour@gmail.com>
2022-05-18 13:43:12 -0400

    started piece object

--- skeleton/src/piece.js
+++ skeleton/src/piece.js
@@ -2,18 +2,25 @@
  * Initializes the Piece with its color.
  */
 function Piece (color) {
+    this.color = color
 }
 
 /**
  * Returns the color opposite the current piece.
  */
 Piece.prototype.oppColor = function () {
+    if(this.color === "black"){
+        this.color = "white"
+    } else {
+        this.color = "black"
+    };
 };
 
 /**
  * Changes the piece's color to the opposite color.
  */
 Piece.prototype.flip = function () {
+    return this.oppColor()
 };
 
 /**
